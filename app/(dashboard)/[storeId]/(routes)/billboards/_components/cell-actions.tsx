04519ca3dd8776bb18de724c55a7ca80260bd116
freshgoldroses <93222569+freshgoldroses@users.noreply.github.com>
2024-07-01 23:54:37 +0200

    Created actions menu

--- app/(dashboard)/[storeId]/(routes)/billboards/_components/cell-actions.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/_components/cell-actions.tsx
@@ -3,6 +3,15 @@
 import { useParams, useRouter } from 'next/navigation';
 import { useState } from 'react';
 import { BillboardColumns } from '@/app/(dashboard)/[storeId]/(routes)/billboards/_components/columns';
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuTrigger
+} from "@/components/ui/dropdown-menu";
+import {Button} from "@/components/ui/button";
+import {Copy, Edit, MoreVerticalIcon, Trash} from "lucide-react";
 
 interface CellActionsProps {
     data: BillboardColumns
@@ -17,7 +26,29 @@ const CellActions = ({ data }: CellActionsProps) => {
 
     return (
         <div>
-            CellActions
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <Button className="w-8 h-8 p-0" variant="ghost">
+                        <span className="sr-only">Open</span>
+                        <MoreVerticalIcon className="h-4 w-4" />
+                    </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end">
+                    <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                    <DropdownMenuItem onClick={() => {}}>
+                        <Copy className="h-4 w-4 mr-2" />
+                        Copy Id
+                    </DropdownMenuItem>
+                    <DropdownMenuItem onClick={() => {}}>
+                        <Edit className="h-4 w-4 mr-2" />
+                        Edit
+                    </DropdownMenuItem>
+                    <DropdownMenuItem onClick={() => {}}>
+                        <Trash className="h-4 w-4 mr-2" />
+                        Delete
+                    </DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
         </div>
     );
 };
