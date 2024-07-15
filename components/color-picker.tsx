318d34d9443a803153520be916606bf9fb1cc071
Alina Makovii <93222569+ya-aliina@users.noreply.github.com>
2024-07-15 23:56:12 +0300

    Added styles for color picker

--- components/color-picker.tsx
+++ components/color-picker.tsx
@@ -1,35 +1,52 @@
 import { useState } from 'react';
 
 import {
-  HsvaColor,
-  hsvaToRgbaString,
-  color as handleColor,
-  validHex,
-  hexToHsva,
-  hsvaToHex,
-  hsvaToHexa,
+    HsvaColor,
+    hsvaToRgbaString,
+    color as handleColor,
+    validHex,
+    hexToHsva,
+    hsvaToHex,
+    hsvaToHexa,
 } from '@uiw/color-convert';
+import { cn } from '@/lib/utils';
 
 import Chrome from '@uiw/react-color-chrome';
 import { GithubPlacement } from '@uiw/react-color-github';
 
 function ColorPicker() {
-  const [hsva, setHsva] = useState({ h:0, s:25.71, v:82.35, a:0.32});
-  const hex = hsvaToHex(hsva)
-  return (
-    <>
-      <div className='w-10 h-10 rounded-full relative' style={{background: hex}}>
-      </div>
+    const [hsva, setHsva] = useState({ h: 0, s: 25.71, v: 82.35, a: 0.32 });
+    const hex = hsvaToHex(hsva)
+    const [isVisible, setIsVisible] = useState(false)
+    return (
+        <>
+            <div
+                className='w-10 h-10 rounded-full relative cursor-pointer' 
+                style={{ background: hex }}
+                onClick={() => setIsVisible(!isVisible)}                
+            >
+            </div>
 
-      <Chrome
-        color={hsva}
-        placement={GithubPlacement.TopLeft}
-        onChange={(color) => {
-          setHsva(color.hsva);
-        }}
-        className='absolute'
-      />
-    </>
-  );
+            <div
+                className={cn(
+                    'absolute !mt-0',
+                    isVisible
+                        ? 'block'
+                        : 'hidden',
+                )}
+            >
+                <Chrome
+                    color={hsva}
+                    placement={GithubPlacement.TopLeft}
+                    onChange={(color) => {
+                        setHsva(color.hsva);
+                    }}
+                    style={{ 'borderRadius': '16px', overflow: 'hidden' }}
+                />
+            </div>
+        </>
+    );
 }
 export default ColorPicker;
+
+// style={{ position: 'absolute', 'marginTop': '0px' }}
